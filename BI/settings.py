557471b87cb9ff58e3e47b3145cdfd5ab94862c0
HenintsoaRakoto <henintsoapatric@gmail.com>
2025-05-17 08:57:37 +0300

    mis à jour du settings.py

--- BI/settings.py
+++ BI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-p*i_q7&=ll0_7hvxpx4r#3a!guh+md7-i8lcpmlwjkt2-lo*ag
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://etl-project-2.onrender.com']
 
 
 # Application definition
@@ -125,7 +126,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static", 
 ]
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
